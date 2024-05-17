43f70d5ce953ee038a4bbd70ade0a065183787a7
Xolmatov1156 <xolmatov1156@gmail.com>
2024-05-17 16:54:06 +0500

    new

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 var elModeBtn = document.querySelector(".header-top__mode-btn")
 var elLogoImg = document.querySelector(".header-top__logo-img")
 
-var elHamburgerBtn = document.querySelector(".header-top__hamburger-btn")
-var elMenuList = document.querySelector(".menu__list")
-
 elModeBtn.addEventListener("click", function() {
     document.body.classList.toggle("mode")
     
@@ -13,8 +10,4 @@ elModeBtn.addEventListener("click", function() {
     else{
         elLogoImg.setAttribute("src", "./images/logo.svg")
     }
-})
-
-elHamburgerBtn.addEventListener("click", function(){
-    elMenuList.classList.toggle("open")
 })
