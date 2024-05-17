41fe280d01c49e00882c107597a956a471260a44
Xolmatov1156 <xolmatov1156@gmail.com>
2024-05-17 18:31:30 +0500

    new

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 var elModeBtn = document.querySelector(".header-top__mode-btn")
 var elLogoImg = document.querySelector(".header-top__logo-img")
 
+var elHamburgerBtn = document.querySelector(".header-top__hamburger-btn")
+var elMenuList = document.querySelector(".menu__list")
+
 elModeBtn.addEventListener("click", function() {
     document.body.classList.toggle("mode")
     
@@ -9,5 +12,9 @@ elModeBtn.addEventListener("click", function() {
     }
     else{
         elLogoImg.setAttribute("src", "./images/logo.svg")
-    }
+    }    
+})
+
+elHamburgerBtn.addEventListener("click", function() {
+    elMenuList.classList.toggle("open")
 })
